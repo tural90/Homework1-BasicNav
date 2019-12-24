27fff7e72f8237b8014297bae20dcfb7f423d789
turalfataliyev <turalfetali@yahoo.com>
2019-12-24 16:16:42 -0500

    TestCases2 class created and executed

--- src/test/java/com/cbt/utilities/StringUtility.java
+++ src/test/java/com/cbt/utilities/StringUtility.java
@@ -8,9 +8,11 @@ public class StringUtility {
 
         if(expected.equals(actual)){
             System.out.println("PASS");
+            System.out.println("URL is the same");
         }
         else {
             System.out.println("FAIL");
+            System.out.println("url is not the same");
         }
 
 
